6dafe05d6c021ce60de21aa18e0891124dcaf287
jorditeixido <jordi@teixido.com>
2020-08-28 19:13:55 +0200

    update

---
+++ Exercici5Linq/JavaScript/js/functions.js
@@ -0,0 +1,24 @@
+/*
+Exercici:
+La fase 1,2 i 3 consisteix en fer servir consultes linq a partir d’aquests numeros 
+( 2, 6, 8, 4, 5, 5, 9, 2, 1, 8 ,7 ,5 ,9 ,6 ,4).
+FASE 1 (2 punts)
+●	Creeu un array amb els números donats al principi de la práctica. (0,5 punts)
+●	Fent servir consultes linq guardeu en un array tots els números parells. (1 punt)
+●	Mostreu l’array de números parells per consola. (0,5 punt)
+
+FASE 2 (2 punts)
+●	Calculeu a partir de l’array anterior la nota mitjana, la máxima i la mínima. (1 punts)
+●	Mostreu per consola els resultats. (1 punt)
+
+FASE 3 (2,5 punts)
+●	Fent una consulta linq guardeu en un array quins numero son mes grans que 5 i en un altre array quins son mes petits. (1,5 punts)
+●	Mostreu per console els dos arrays. (1 punt)
+
+FASE 4 (3,5 punts)
+En aquesta fase farem servir un array amb noms: (David, Sergio, Maria, Laura, Oscar, Julia, Oriol)
+●	Creeu un array amb els noms de l’enunciat. (0,5 punts)
+●	Fent servir consultes linq mostreu per consola els noms que comencin amb la lletra “O”. (1 punt)
+●	Fent servir consultes linq mostreu per consola quins noms tenen més de 6 lletres. (1 punt)
+●	Fent servir consultes linq mostreu per consola els noms de l’array ordenats de manera descendent.         (1 punt)
+*/
